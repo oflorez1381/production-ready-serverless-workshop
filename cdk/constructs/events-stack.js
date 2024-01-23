c851a54671635215efc3c3532deb4f9002790696
oflorez1381 <oscarflorez1381@gmail.com>
2024-01-23 18:55:43 -0500

    feat: Exercise - Include SNS and EventBridge in end-to-end tests

--- cdk/constructs/events-stack.js
+++ cdk/constructs/events-stack.js
@@ -1,8 +1,10 @@
-const { Stack } = require('aws-cdk-lib')
-const { EventBus, Rule } = require('aws-cdk-lib/aws-events')
-const { LambdaFunction } = require('aws-cdk-lib/aws-events-targets')
-const { Topic } = require('aws-cdk-lib/aws-sns')
+const { Stack, Duration, CfnOutput } = require('aws-cdk-lib')
+const { EventBus, Rule, RuleTargetInput, EventField } = require('aws-cdk-lib/aws-events')
+const { LambdaFunction, SqsQueue } = require('aws-cdk-lib/aws-events-targets')
+const { Topic, Subscription } = require('aws-cdk-lib/aws-sns')
 const { Function, Code, Runtime } = require('aws-cdk-lib/aws-lambda')
+const { Queue } = require('aws-cdk-lib/aws-sqs')
+const { PolicyStatement, ServicePrincipal } = require('aws-cdk-lib/aws-iam')
 
 class EventsStack extends Stack {
     constructor(scope, id, props) {
@@ -36,6 +38,68 @@ class EventsStack extends Stack {
             }
         })
         rule.addTarget(new LambdaFunction(notifyRestaurantFunction))
+
+        const isE2eTest = props.stageName.startsWith('dev')
+        if (isE2eTest) {
+            this.declareTestResources(restaurantNotificationTopic, orderEventBus)
+        }
+    }
+
+    declareTestResources(restaurantNotificationTopic, orderEventBus) {
+        const testQueue = new Queue(this, 'E2eTestQueue', {
+            retentionPeriod: Duration.seconds(60),
+            visibilityTimeout: Duration.seconds(1)
+        })
+
+        testQueue.addToResourcePolicy(new PolicyStatement({
+            actions: ['sqs:SendMessage'],
+            resources: [testQueue.queueArn],
+            principals: [new ServicePrincipal('sns.amazonaws.com')],
+            conditions: {
+                ArnEquals: {
+                    'aws:SourceArn': restaurantNotificationTopic.topicArn,
+                }
+            }
+        }))
+
+        testQueue.addToResourcePolicy(new PolicyStatement({
+            actions: ['sqs:SendMessage'],
+            resources: [testQueue.queueArn],
+            principals: [new ServicePrincipal('events.amazonaws.com')],
+            conditions: {
+                ArnEquals: {
+                    'aws:SourceArn': orderEventBus.eventBusArn,
+                }
+            }
+        }))
+
+        new Subscription(this, 'E2eTestSnsSubscription', {
+            topic: restaurantNotificationTopic,
+            protocol: 'sqs',
+            endpoint: testQueue.queueArn,
+            rawMessageDelivery: false
+        })
+
+        const sqsRule = new Rule(this, 'SqsRule', {
+            eventBus: orderEventBus,
+            eventPattern: {
+                source: ['big-mouth']
+            }
+        })
+        sqsRule.addTarget(new SqsQueue(testQueue, {
+            message: RuleTargetInput.fromObject({
+                event: {
+                    source: EventField.source,
+                    'detail-type': EventField.detailType,
+                    detail: EventField.fromPath('$.detail')
+                },
+                eventBusName: orderEventBus.eventBusName
+            })
+        }))
+
+        new CfnOutput(this, 'E2eTestQueueUrl', {
+            value: testQueue.queueUrl
+        })
     }
 }
 
--- tests/steps/when.js
+++ tests/steps/when.js
@@ -1,3 +1,4 @@
+const { EventBridgeClient, PutEventsCommand } = require('@aws-sdk/client-eventbridge')
 const aws4 = require('aws4')
 const URL = require('url')
 const http = require('axios')
@@ -5,6 +6,19 @@ const http = require('axios')
 const mode = process.env.TEST_MODE
 const APP_ROOT = '../../'
 const _ = require('lodash')
+
+const viaEventBridge = async (busName, source, detailType, detail) => {
+  const eventBridge = new EventBridgeClient()
+  const putEventsCmd = new PutEventsCommand({
+    Entries: [{
+      Source: source,
+      DetailType: detailType,
+      Detail: JSON.stringify(detail),
+      EventBusName: busName
+    }]
+  })
+  await eventBridge.send(putEventsCmd)
+}
 
 const viaHandler = async (event, functionName) => {
   const handler = require(`${APP_ROOT}/functions/${functionName}`).handler
@@ -123,7 +137,8 @@ const we_invoke_notify_restaurant = async (event) => {
   if (mode === 'handler') {
     await viaHandler(event, 'notify-restaurant')
   } else {
-    throw new Error('not supported')
+    const busName = process.env.bus_name
+    await viaEventBridge(busName, event.source, event['detail-type'], event.detail)
   }
 }
 
